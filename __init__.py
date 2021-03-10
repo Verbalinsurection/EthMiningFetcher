1835dab2a7c9fb8bae90209786280e598e038c84
Thomas Stassinopoulos <t.stassinopoulos@stass.eu>
2021-03-10 23:54:14 +0100

    Update module declaration

---
+++ __init__.py
@@ -0,0 +1,8 @@
+from .coin_market import Coin
+from .coincalculators import CoinCalculators
+from .ethermine_api import Ethermine
+from .etherscan import EtherWallet
+from .ethpay import EthPay
+
+__all__ = ['Coin', 'CoinCalculators', 'Ethermine', 'EtherWallet', 'EthPay']
+__version__ = '1.0'
